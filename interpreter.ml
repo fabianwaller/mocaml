5828606d75da0e3d329faa8ce46670afe38cc913
fabianwaller <fabianwaller@icloud.com>
2021-12-18 15:21:33 +0100

    parser

--- interpreter.ml
+++ interpreter.ml
@@ -3,40 +3,6 @@
   Fabian Waller
 *)
 
-(* Expressions, Types, Environments *)
-
-type ty = Bool | Int | Arrow of ty * ty
-type con = Bcon of bool | Icon of int
-type op = Add | Sub | Mul | Leq
-type var = string
-type exp = Var of var | Con of con
-          | Oapp of op * exp * exp
-          | Fapp of exp * exp
-          | If of exp * exp * exp
-          | Lam of var * exp
-          | Lamty of var * ty * exp
-          | Let of var * exp * exp
-          | Letrec of var * var * exp * exp
-          | Letrecty of var * var * ty * ty * exp * exp
-
-let expFact = 
-  Letrec("fact","x"
-      ,If(Oapp(Leq,Var "x",Con (Icon 1))
-          ,Con (Icon 1)
-          ,Oapp(Mul,Var "x",Fapp(Var "fact",Oapp(Sub,Var "x",Con (Icon 1)))))
-      ,Fapp(Var "fact",Con(Icon 10)))
-
-
-type ('a,'b) env = ('a * 'b) list
-let empty : ('a,'b) env = []
-let update (env : ('a,'b) env) a b : ('a,'b) env = (a,b) :: env
-let rec lookup (env : ('a,'b) env) a = match env with
-  | (a',b) :: env -> if a' = a then Some b else lookup env a
-  | [] -> None
-
-
-
-
 (* Lexer *)
 
 type const = BCON of bool | ICON of int
@@ -102,11 +68,107 @@ let lex s : token list =
 
   in lex' 0 []
 
-let fac_string =
+let test_string =
   "let rec fac a = fun n ->
     if n <= 1 then a else fac (n*a) (n-1) 
     in fac 1 7"
-let test = lex fac_string = [LET; REC; VAR "fac"; VAR "a"; EQ; LAM; VAR "n"; ARR; IF; VAR "n"; LEQ;
+let test = lex test_string = [LET; REC; VAR "fac"; VAR "a"; EQ; LAM; VAR "n"; ARR; IF; VAR "n"; LEQ;
 CON (ICON 1); THEN; VAR "a"; ELSE; VAR "fac"; LP; VAR "n"; MUL; VAR "a"; RP;
 LP; VAR "n"; SUB; CON (ICON 1); RP; IN; VAR "fac"; CON (ICON 1);
 CON (ICON 7)]
+
+
+(* Expressions, Types, Environments *)
+
+type ty = Bool | Int | Arrow of ty * ty
+type con = Bcon of bool | Icon of int
+type op = Add | Sub | Mul | Leq
+type var = string
+type exp = Var of var | Con of con
+          | Oapp of op * exp * exp
+          | Fapp of exp * exp
+          | If of exp * exp * exp
+          | Lam of var * exp
+          | Lamty of var * ty * exp
+          | Let of var * exp * exp
+          | Letrec of var * var * exp * exp
+          | Letrecty of var * var * ty * ty * exp * exp
+
+let expFact = 
+  Letrec("fact","x"
+      ,If(Oapp(Leq,Var "x",Con (Icon 1))
+          ,Con (Icon 1)
+          ,Oapp(Mul,Var "x",Fapp(Var "fact",Oapp(Sub,Var "x",Con (Icon 1)))))
+      ,Fapp(Var "fact",Con(Icon 10)))
+
+
+type ('a,'b) env = ('a * 'b) list
+let empty : ('a,'b) env = []
+let update (env : ('a,'b) env) a b : ('a,'b) env = (a,b) :: env
+let rec lookup (env : ('a,'b) env) a = match env with
+  | (a',b) :: env -> if a' = a then Some b else lookup env a
+  | [] -> None
+
+
+
+(* Parser *)
+let parse l = 
+  let verify c l = match l with 
+    | [] -> []
+    | c'::l -> if c = c' then l else failwith "verify: wrong token" in
+  let rec exp l : exp * token list = match l with (* top level *)
+    | IF::l -> 
+      let (e1,l) = exp l in
+      let (e2,l) = exp (verify THEN l) in
+      let (e3,l) = exp (verify ELSE l) in
+      (If(e1,e2,e3),l)
+    | LAM::VAR x::ARR::l -> 
+      let(e,l) = exp l in 
+      (Lam(x,e),l)
+    (*| LAM::LP::VAR x::COL::l -> 
+      let(ty,l) = type l in 
+      let(e,l) = exp l in Lamty(x, ty, e)*)
+    | LET::VAR x::EQ::l -> 
+      let(e1,l) = exp l in
+      let(e2,l) = exp (verify IN l) in
+      (Let(x,e1,e2),l)
+    | LET::REC::VAR f::VAR x::EQ::l -> 
+      let(e1,l) = exp l in
+      let(e2,l) = exp (verify IN l) in
+      (Letrec(f,x,e1,e2),l)
+    (*| LET::REC::VAR f::LP::VAR x::COL::l -> *) 
+    | l -> cexp l
+
+  and cexp l = let (e1,l) = sexp l in cexp' e1 l (* comparisons, infix *)
+  and cexp' e1 l = match l with
+      | LEQ::l -> let(e2,l) = sexp l in (Oapp(Leq,e1,e2),l)
+      | l -> (e1,l)
+
+  and sexp l = let (e1,l) = mexp l in sexp' e1 l (* additive operators, infix *)
+  and sexp' e1 l = match l with (* auxilary categorie to realize left-associativity *)
+      | ADD::l -> let (e2,l) = mexp l in sexp' (Oapp(Add,e1,e2)) l 
+      | SUB::l -> let (e2,l) = mexp l in sexp' (Oapp(Sub,e1,e2)) l
+      | l -> (e1,l)
+
+  and mexp l = let (e1,l) = aexp l in mexp' e1 l (* multiplicative operators, infix *)
+  and mexp' e1 l = match l with (* auxilary categorie to realize left-associativity *)
+      | MUL::l -> let(e2,l) = aexp l in aexp' (Oapp(Mul,e1,e2)) l (* why aexp' here?? *)
+      | l -> (e1,l)
+
+  and aexp l = let (e1,l) = pexp l in aexp' e1 l (* function applications, infix *)
+  and aexp' e1 l = match l with (* auxilary categorie to realize left-associativity *)
+    | VAR _ :: _ | CON _ :: _ | LP :: _ -> let(e2,l) = pexp l in aexp' (Fapp(e1,e2)) l
+    | l -> (e1,l)
+
+  and pexp l = match l with (* bottom level, takes care of variables, constants, and parenthesized expressions *)
+    | VAR x::l -> (Var x,l)
+    | CON (BCON b)::l -> (Con (Bcon b), l)
+    | CON (ICON n)::l -> (Con (Icon n), l)
+    | LP:: l -> let (e,l) = exp l in (e, verify RP l)
+    | _ -> failwith ("parsing: pexp")
+
+  in exp l
+
+
+
+let test = parse (lex test_string)
